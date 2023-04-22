9b921cd79fa934a83a27f0800860e870cb22a493
Julien Biezemans <jb@jbpros.com>
2023-04-22 21:50:14 +0200

    Set up LSP signature

--- plugins/user.lua
+++ plugins/user.lua
@@ -2,11 +2,15 @@ return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
+  {
+    "ray-x/lsp_signature.nvim",
+    event = "BufRead",
+    config = function()
+      require("lsp_signature").setup()
+    end,
+    bind = true, -- This is mandatory, otherwise border config won't get registered.
+    handler_opts = {
+      border = "rounded"
+    }
+  },
 }
