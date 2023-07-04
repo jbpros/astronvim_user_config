d6721b797137a25edeeed89985cf4031e347d857
Julien Biezemans <jb@jbpros.com>
2023-07-04 20:32:13 +0200

    Add vim-pug

--- plugins/user.lua
+++ plugins/user.lua
@@ -41,5 +41,9 @@ return {
         vim.keymap.set("n", "K", require("hover").hover, {desc = "hover.nvim"})
         vim.keymap.set("n", "gK", require("hover").hover_select, {desc = "hover.nvim (select)"})
     end
+  },
+  {
+    "digitaltoad/vim-pug",
+    ft = "pug",
   }
 }
