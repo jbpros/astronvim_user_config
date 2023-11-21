514e2cea24e0593a0153fb54cd0374f1263cdd05
Julien Biezemans <jb@jbpros.com>
2023-11-21 08:46:56 +0100

    Get TS diagnostics on changes instead of leave_insert

--- plugins/typescript.lua
+++ plugins/typescript.lua
@@ -2,5 +2,9 @@ return {
   "pmizio/typescript-tools.nvim",
   lazy = false,
   dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-  opts = {},
+  opts = {
+    settings = {
+      publish_diagnostic_on = "change"
+    },
+  },
 }
